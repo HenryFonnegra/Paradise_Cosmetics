470e7daa5793983cb5e9ba188cafb7840305cb4a
BienavNet <bienavnet@gmail.com>
2021-10-31 12:11:02 -0500

    jerson1

--- views/index/index.php
+++ views/index/index.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <!--<link rel="stylesheet" href="<?= constant('URL') ?>plugins/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="<?= constant('URL') ?>plugins/css/bootstrap.min.css">
 
     <!-- FontAwesome CSS -->
     <link rel="stylesheet" href="<?= constant('URL') ?>plugins/css/font-awesome.min.css">
