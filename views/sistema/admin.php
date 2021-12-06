88ec7b2ec34c2e67047324119efb3bd07ca3f9e8
PacP101 <cristiancperez@unicesar.edu.co>
2021-12-06 11:16:23 -0500

    asd

--- views/sistema/admin.php
+++ views/sistema/admin.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="<?=constant('URL')?>plugins/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="<?=constant('URL')?>https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/plugins/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body class="px-4 py-4">
@@ -45,6 +45,6 @@
         </div>
       </form>
 </div>
-    <script src="<?=constant('URL')?>plugins/bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="<?=constant('URL')?>https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/plugins/bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
