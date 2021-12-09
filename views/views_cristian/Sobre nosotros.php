ec792e66f23bc9d78037dccf688785b90bfcabbb
PacP101 <cristiancperez@unicesar.edu.co>
2021-12-09 16:53:30 -0500

    sobrenosotros

---
+++ views/views_cristian/Sobre nosotros.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Contactanos</title>
+        <link rel="icon" type="image/x-icon" href="assets/logo.jpeg" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles2.css" rel="stylesheet" />
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <!-- FontAwesome CSS -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+        <!-- Swiper CSS -->
+        <link rel="stylesheet" href="css/swiper.min.css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+       <!-- <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div class="container px-5">
+                <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#!">Sign Up</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Log In</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav> -->
+        <!-- Header-->
+        <!--<header>
+            <img src="images/contacto.png" height="600px" width="100%" alt="..." />
+        </header> -->
+
+        <header class="site-header">
+            <div class="header-bar">
+                <div class="container-fluid">
+                    <div class="row align-items-center">
+                        <div class="col-10 col-lg-2 order-lg-1">
+                            <div class="site-branding">
+                                <div class="site-title">
+                                    <a href="#"><img src="images/logo 1.png" alt="logo"></a>
+                                </div><!-- .site-title -->
+                            </div><!-- .site-branding -->
+                        </div><!-- .col -->
+        
+                        <div class="col-2 col-lg-7 order-3 order-lg-2">
+                            <nav class="site-navigation">
+                                <div class="hamburger-menu d-lg-none">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div><!-- .hamburger-menu -->
+        
+                                <ul>
+                                    <li><a href="index.html">Inicio</a></li>
+                                    <li><a href="#">Sobre Nosotros</a></li>
+                                    <li><a href="#">Eventos</a></li>
+                                    <li><a href="#">Noticias</a></li>
+                                </ul>
+                            </nav><!-- .site-navigation -->
+                        </div><!-- .col -->
+                    </div><!-- .row -->
+                </div><!-- .container-fluid -->
+            </div><!-- .header-bar -->
+        
+            <div class="page-header contact-page-header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-10">
+                            <header class="entry-header">
+                                <h1 class="entry-title">Sobre Nosotros.</h1>
+                            </header>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header><!-- .site-header -->
+
+        <!-- Content section 1-->
+        <section id="scroll">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5"><img class="img-fluid rounded-circle" src="images/au1.png" alt="..." /></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-3">
+                            <h2 class="display-4"> Paradise Cosmetics</h2>
+                            <p>Paradise Comestic es una tienda de comesticos de belleza ubicada en pelaya-Cesar,la idea de hacer una tienda virtual
+                                 de productos de belleza nace con el fin de poder llegarle a todas aquellas mujeres
+                                 que necesiten estos productos tan maravillosos.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div style="height: 10px; background-color: black;" ></div>
+       
+        <!-- Footer-->
+        <footer class="py-5 bg-black">
+            <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; Your Website 2021</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+
+
